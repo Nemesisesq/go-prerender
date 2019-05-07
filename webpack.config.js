fb3b11a0aa8dd0f3c0e0e16f65d300d2aa463ce1
Nemesisesq <nemesisesq@gmail.com>
2019-05-07 00:52:00 -0400

    added index entry point

--- webpack.config.js
+++ webpack.config.js
@@ -25,56 +25,57 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				include: [path.resolve(__dirname, 'src')],
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				test: /\.js$/
-			},
-			{
-				test: /\.(scss|css)$/,
+    module: {
+        rules: [
+            {
+                include: [path.resolve(__dirname, 'src')],
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                test: /\.js$/
+            },
+            {
+                test: /\.(scss|css)$/,
 
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader'
-					},
-					{
-						loader: 'sass-loader'
-					}
-				]
-			}
-		]
-	},
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
 
-	entry: {
-	  server: './src/server'
-	},
+    entry: {
+        server: './src/server.js',
+        index: './src/index.js'
+    },
 
-	output: {
-		chunkFilename: '[name].[chunkhash].js',
-		filename: '[name].[chunkhash].js'
-	},
+    output: {
+        chunkFilename: '[name].[chunkhash].js',
+        filename: '[name].[chunkhash].js'
+    },
 
-	mode: 'development',
+    mode: 'development',
 
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    priority: -10,
+                    test: /[\\/]node_modules[\\/]/
+                }
+            },
 
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	}
+            chunks: 'async',
+            minChunks: 1,
+            minSize: 30000,
+            name: true
+        }
+    }
 };
